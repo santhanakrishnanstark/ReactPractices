69c341519362215068c0d414bba58cc499d1017d
santhanakrishnan <santhanakrishnanstark@gmail.com>
2018-05-23 18:47:27 +0530

    Lesson2.1 conditional statement

--- src/App.js
+++ src/App.js
@@ -8,30 +8,59 @@ class App extends Component {
       person : [
         {name:'sandy',msg:'am a programmer'},
          {name:'stark',msg:'am an inventor'}
-      ]
+      ],
+      showPerson : false
     }
 
-  reactHandler = () =>{
-   
+  toggleHandler = () =>{
+    const doeshow = this.state.showPerson;
+    this.setState({showPerson:!doeshow});
   }
   changeHandler = (event) =>{
     this.setState({
       person:[
         {name:event.target.value, msg:'am a programmer'},
-        {name:event.target.value, msg:'am an inventor'}
+        {name:'stark', msg:'am an inventor'}
       ]
     })
   }
 
+  deleteHandler = (personindex) => {
+    const persons=this.state.person;
+    persons.splice(personindex,1);
+    this.setState({ person : persons});
+  }
+
   render() {
+
+    let person = null; 
+    if(this.state.showPerson){
+      person = (
+        <div>
+        { this.state.person.map((person,index) =>{
+          return <Person click={()=>this.deleteHandler(index)}
+                           name={person.name} msg={person.msg} 
+                           changed={this.changeHandler}
+          />
+        }) }
+        </div>
+
+         
+           /* <div>
+            <Person name={this.state.person[0].name} msg={this.state.person[0].msg}
+                    changed={this.changeHandler}/>
+            <Person name={this.state.person[1].name} msg={this.state.person[1].msg}
+                    changed={this.changeHandler}>I am an Avenger Do you ?</Person>
+          </div>  */
+      );
+    }
+
     return (
       <div className='container'>
         <h2 className='rc'>This is React Js</h2>
-        <button onClick={this.reactHandler}>React</button>
-        <Person name={this.state.person[0].name} msg={this.state.person[0].msg}
-                changed={this.changeHandler}/>
-        <Person name={this.state.person[1].name} msg={this.state.person[1].msg}
-                changed={this.changeHandler}>I am an Avenger Do you ?</Person>
+        <button onClick={this.toggleHandler}>React</button>
+        {person}
+         
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) =>{
     return(
         <div className='card'>
-            <h2>{props.name}</h2>
+            <h2 onClick={props.click}>{props.name}</h2>
             <p>{props.msg}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/> 
